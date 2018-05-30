bf6efd43992722e47b3a63f93c78fd428a90d959
Adrian <atapia@miespacioseguro.cl>
2018-05-30 18:17:58 -0400

    first commmit

---
+++ bin/main.dart
@@ -0,0 +1,75 @@
+
+
+main(List<String> arguments) { //entry point for execution
+
+  // Variable = spot in memory. Or just a bucket to hold a value.
+
+  //Type of variable??
+    /*
+      String
+      Number (integers and doubles)
+      Boolean - 1 or 0 , true or false
+      lists
+      maps
+      ...
+
+      Const and final keywords
+      const = we use this keyword when we want the value/variable to be a constant
+      final - if we want a varibale/value to always be constant = never changes
+
+      Concatenation = use $
+      Equality and relational =  ==, !=, <, >, >= , <=
+     */
+
+//      String name = "Adrian";
+//      String apellidoPaterno = "Tapia";
+//      int rut = 18604258;
+//      int dv = 1;
+//
+//      print("El nombre es $name $apellidoPaterno y su rut es $rut - $dv");
+//      print("Buena $name ${apellidoPaterno.toUpperCase()}");
+//      print("Buena $name ${apellidoPaterno.toLowerCase()}");
+//
+//      print("Operadores");
+//
+//      int number = 24;
+//      int numberTwo = 2;
+//      var result = number + numberTwo;
+//      double pi = 3.14;
+//      double gravity = 2.10;
+//
+//      print(number + numberTwo);
+//      print("El resultado es: $result");
+//      print(pi - gravity);
+//      print(4%2);
+
+      print("Igualdad y relaciones");
+      print("---------------------");
+
+      int number = 24;
+      int numberTwo = 24;
+
+      print(number <= numberTwo) ;
+
+//  bool isTrue = true;
+//  bool isFalse = false;
+//
+//  print(isFalse);
+
+  /*var country ;
+  country = 23;
+  country = "Chile";
+
+  print(country);
+
+  String name;
+  name = "Adrian Tapia";
+
+  print('Hello world');
+  print(name);
+
+  int age = 24;
+  double number = 23.5;
+  print(number);*/
+
+}
